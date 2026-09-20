42b668cc4a6b076f54030a01839d0860acd96be9
fishdawg90 <mckibbon.ross@gmail.com>
2026-09-20 19:35:25 +0100

    Improve pantry photo review and spice tracking

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'our-basket-phone-v12';
+const CACHE = 'our-basket-phone-v13';
 const ROOT = self.registration.scope;
 self.addEventListener('install', event => {
   event.waitUntil((async () => {
