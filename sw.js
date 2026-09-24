072923a9dae4bffe4d31be9351e4218ff38d5cf6
fishdawg90 <mckibbon.ross@gmail.com>
2026-09-24 20:29:23 +0100

    Improve pantry rebuy learning

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'our-basket-phone-v14';
+const CACHE = 'our-basket-phone-v15';
 const ROOT = self.registration.scope;
 self.addEventListener('install', event => {
   event.waitUntil((async () => {
